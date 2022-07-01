992fbd780b327fedfa518881bd2a82e75bde568b
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-07-01 12:22:55 +0900

    独自ドメインをcorsのoriginsに追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://sveltekit-blog-app-kensuke.vercel.app", "http://localhost:3000"
+    origins "https://sveltekit-blog-app-kensuke.vercel.app", "http://localhost:3000", "https://sveltekit-blog-app-kensuke.xyz"
 
     resource "*",
       headers: :any,
